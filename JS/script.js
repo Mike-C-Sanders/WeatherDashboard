124eb2a15e9a75630b5ac6aa9559149fe9bc21ab
Mike Sanders <msanders331@gmail.com>
2022-02-01 07:15:49 -0800

    Added in the api call to weather map.org. Next weill be updating the DOM to reflect the call made.

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,17 @@
-var apiKey = 'ec3531a4604c1c9d7f8f0e0ac90c2251'
+// Api variables from https://openweathermap.org/
+var apiKey = '&appid=ec3531a4604c1c9d7f8f0e0ac90c2251'
+var apiCityToday = 'api.openweathermap.org/data/2.5/weather?q='
 
+// Global variables
+var city = 'London';
+var apiRequest = `${apiCityToday}${city}${apiKey}`
+
+console.log(apiRequest);
+
+var todaysWeather = fetch('api.openweathermap.org/data/2.5/weather?q=London&appid=ec3531a4604c1c9d7f8f0e0ac90c2251').then(function(response){
+        console.log(response.json());
+    }).catch(function(error){
+        console.log('there was an error:', error);
+    })
+    
+// todaysWeather();
