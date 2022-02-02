fd71b537edf9204eaedf4b467c9aac365843e871
Mike Sanders <msanders331@gmail.com>
2022-02-01 23:38:14 -0800

    added some updated icon when user searches. Need to adjust size, at this time it's taking up the whole screen.

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 // Api variables from https://openweathermap.org/
 var apiKey = '&appid=ec3531a4604c1c9d7f8f0e0ac90c2251'
 var apiCityToday = 'https://api.openweathermap.org/data/2.5/weather?q='
+var apiCity5Day = 'https://api.openweathermap.org/data/2.5/forecast?q='
 
 // Global variables
 var searchBtn = document.querySelector('#search-button');
@@ -9,10 +10,13 @@ var todayHeader = document.querySelector('#today-header');
 var todaysTemp = document.querySelector('#todays-temp');
 var todaysWind = document.querySelector('#todays-wind');
 var todaysHumidity = document.querySelector('#todays-humidity');
-var todaysUVI = document.querySelector('#todays-uvi');
-var city;
+var todaysPressure = document.querySelector('#todays-pressure');
+var todayIcon =document.querySelector('#today-icon');
 
-//Get today's weather
+var city;
+// var storedSearch = localStorage.getItem(JSON.stringify)
+
+
 //Get 5-day forecast
 
 //when a user searches for a city.
@@ -24,25 +28,54 @@ var searchForCity = function(){
 
         city = searchCity.value;
 
-        var apiRequest = `${apiCityToday}${city}&units=imperial${apiKey}`;
-
-        fetch(apiRequest).then(function(response){
+        var apiRequestToday = `${apiCityToday}${city}&units=imperial${apiKey}`;
+        var apiRequest5Day = `${apiCity5Day}${city}&units=imperial${apiKey}`
+        //Get today's weather
+        fetch(apiRequestToday).then(function(response){
         
-                return response.json();
+            return response.json();
             }).then(function(json){
+
                 updateTodaysWeather(json);
+
             }).catch(function(error){
                 console.log('there was an error:', error);
             })
-            
+
+        fetch(apiRequest5Day).then(function(response){
+            return response.json();
+        }).then(function(json){
+
+        })
+        
     })
 }
 
 var updateTodaysWeather = function(data){
-    todayHeader.textContent = `${data.name} `
+    //Set text areas for today's weather
+    todayHeader.textContent = `${data.name}`
+    todayIcon.setAttribute('src',`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+    
+    todaysTemp.textContent = `Temp: ${data.main.temp} \xB0F`
+    todaysWind.textContent = `Wind: ${data.wind.speed} MPH`
+    todaysHumidity.textContent = `Humidity: ${data.main.humidity}%`
+    todaysPressure.textContent = `Pressure: ${data.main.pressure} hPa`
+
+    // localStorage.setItem(data.name);
+}
+
+var update5DayForecast = function(data){
 
 
 
 }
 
+var clearCardText = function(){
+    var card = document.querySelectorAll('.card-text');
+    card.forEach(function(i){
+        i.textContent = '';
+    })
+}
 searchForCity();
+
+// clearCardText();
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # WeatherDashboard
 
 Custom built weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.  Use the [OpenWeather One Call API](https://openweathermap.org/api/one-call-api) to retrieve weather data for cities.
+
+
